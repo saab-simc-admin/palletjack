3bd0e772d29cdef37f24f548af449782075fe5ad
Calle Englund <calle.englund@saabgroup.com>
2016-10-21 14:41:40 +0200

    Change pallet fetch APIs to match the filtering for KVDAG objects. This breaks everything, and bumps gem minor version.

--- lib/palletjack.rb
+++ lib/palletjack.rb
@@ -34,83 +34,32 @@ class PalletJack
 
   # Search for pallets in a PalletJack warehouse
   #
-  # :call-seq:
-  #   [kind]                        -> set of pallets
-  #   [kind, name: pallet_name]     -> set of pallets
-  #   [kind, with_all:{ matches }]  -> set of pallets
-  #   [kind, with_any:{ matches }]  -> set of pallets
-  #   [kind, with_none:{ matches }] -> set of pallets
+  # The search is filtered by KVDAG::Vertex#match? expressions.
   #
-  # Return all pallets of +kind+, optionally restricting by
-  # keypath matches.
+  # Useful Pallet methods to match for include:
   #
-  # +pallet_name+ is the filesystem +basename+ for the pallet.
-  #
-  # +matches+ should be a hash with "key.path" strings as keys,
-  # and either string or regexp values to match against.
+  # kind:: the kind of pallet
+  # name:: the filesystem +basename+ of the pallet
 
-  def [](kind, options = {})
-    match_enumerator = {
-      :with_all  => :all?,
-      :with_any  => :any?,
-      :with_none => :none?
-    }
-    result = Set.new
-
-    case
-    when options.empty?
-      result += @pallets[kind].values
-    when options[:name]
-      if p = @pallets[kind][options[:name]]
-        result << p
-      end
-    else
-      match_enumerator.keys.each do |tag|
-        if options[tag]
-          @pallets[kind].each do |_, pallet|
-            if options[tag].send(match_enumerator[tag]) do |key, value|
-                case value
-                when Regexp
-                  pallet[key] =~ value
-                else
-                  pallet[key].to_s == value.to_s
-                end
-              end
-            then
-              result << pallet
-            end
-          end
-        end
-      end
-    end
-
-    result
+  def [](filter = {})
+    @dag.vertices(filter)
   end
 
   # Fetch a single pallet from a PalletJack warehouse
   #
-  # :call-seq:
-  #   fetch(kind)                        -> pallet or KeyError
-  #   fetch(kind, name: name)            -> pallet or KeyError
-  #   fetch(kind, with_all:{ matches })  -> pallet or KeyError
-  #   fetch(kind, with_any:{ matches })  -> pallet or KeyError
-  #   fetch(kind, with_none:{ matches }) -> pallet or KeyError
+  # The search is filtered by KVDAG::Vertex#match? expressions.
   #
-  # Return exactly one pallet of +kind+, optionally restricting by
-  # keypath matches.
+  # Useful Pallet methods to match for include:
   #
-  # Raise a KeyError if there is more or less than one match.
-  #
-  # +matches+ should be a hash with "key.path" strings as keys,
-  # and either string or regexp values to match against.
-  #
+  # kind:: the kind of pallet
+  # name:: the filesystem +basename+ of the pallet
 
-  def fetch(kind, options = {})
-    result = self[kind, options]
-    if result.length != 1 then
-      raise KeyError.new("\"#{kind}\", #{options} matched #{result.length} pallets")
+  def fetch(filter = {})
+    result = self[filter]
+
+    if result.length != 1
+      raise KeyError.new("#{options} matched #{result.length} pallets")
     end
-
     result.first
   end
 end
