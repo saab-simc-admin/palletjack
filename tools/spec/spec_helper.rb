6e6744c945cf5bcedc64694112d59f44d4d141c9
Karl-Johan Karlsson <karl-johan.karlsson@saabgroup.com>
2016-10-26 17:50:06 +0200

    Basic definitions for test cases

---
+++ tools/spec/spec_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path("../../exe", __FILE__)
+
+$EXAMPLE_WAREHOUSE = File.expand_path('../../../examples/warehouse', __FILE__)
