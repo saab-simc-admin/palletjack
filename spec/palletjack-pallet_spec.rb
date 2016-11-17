2e9d8c0fb7f90049bb229f03b6508756ac2d6a40
Karl-Johan Karlsson <karl-johan.karlsson@saabgroup.com>
2016-11-17 15:49:52 +0100

    Update test cases to expect everything to be strings
    Primitive types such as integers and booleans cannot be extended with
    source position information, so all data read from a pallet object is
    now strings. Update test cases to match.

--- spec/palletjack-pallet_spec.rb
+++ spec/palletjack-pallet_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 require 'rspec_structure_matcher'
+require 'traceable'
 
 describe PalletJack::Pallet do
   before :context do
     @jack = PalletJack.load($EXAMPLE_WAREHOUSE)
   end
-  
+
   it 'can load contents of a pallet' do
     expect {
       path = File.join('domain', 'example.com')
@@ -13,7 +14,7 @@ describe PalletJack::Pallet do
       PalletJack::Pallet.load(@jack, identity)
     }.not_to raise_error
   end
-  
+
   it 'cannot load contents of a non-existent pallet' do
     expect {
       path = File.join('__invalid__', '__no_such__')
@@ -21,25 +22,32 @@ describe PalletJack::Pallet do
       PalletJack::Pallet.load(@jack, identity)
     }.to raise_error Errno::ENOENT
   end
-  
+
   context 'a loaded pallet' do
     before :context do
       path = File.join('domain', 'example.com')
       identity = PalletJack::Pallet::Identity.new(@jack, path)
       @pallet = PalletJack::Pallet.load(@jack, identity)
     end
-    
+
     it 'has a kind' do
       expect(@pallet.kind).to match 'domain'
     end
-    
+
     it 'has a name' do
       expect(@pallet.name).to match 'example.com'
     end
-    
+
     it 'serializes into YAML of its key contents' do
       pallet_hash = @pallet.to_hash
-      yaml_hash = YAML::load(@pallet.to_yaml)
+
+      handler = PositionHandler.new('@pallet')
+      parser = Psych::Parser.new(handler)
+      handler.parser = parser
+      parser.parse(@pallet.to_yaml)
+      visitor = PositionVisitor.new
+      yaml_hash = visitor.accept(handler.root)[0]
+
       expect(yaml_hash).to have_structure pallet_hash
     end
   end
